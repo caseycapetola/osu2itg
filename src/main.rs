de31c8cf4e6f50c1226c354a5e8ff42dd050a0db
Casey Capetola <caseycapetola@gmail.com>
2024-11-17 20:26:03 -0500

    Added Song struct and more file parsing logic

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,51 @@ mod osu_parser;
 // use std::fs::File;
 use crate::osu_parser::OsuParser;
 
+struct Song {
+    title: String,
+    subtitle: String,
+    artist: String,
+    credit: String, // simfile author
+    music: String,
+    banner: String,
+    background: String,
+    sample_start: String,
+    sample_length: String,
+}
+
+impl Song {
+    pub fn new(title: String, artist: String, credit: String, subtitle: String) -> Song {
+        Song {
+            title,
+            subtitle,
+            artist,
+            credit,
+            music: String::with_capacity(128),
+            banner: String::with_capacity(128),
+            background: String::with_capacity(128),
+            sample_start: String::with_capacity(128),
+            sample_length: String::with_capacity(128),
+        }
+    }
+
+    pub fn get_song_details(&self) -> String {
+        format!("Title: {}\nSubtitle: {}\nArtist: {}\nCredit: {}\nMusic: {}\nBanner: {}\nBackground: {}\nSample Start: {}\nSample Length: {}", 
+            self.title, self.subtitle, self.artist, self.credit, self.music, self.banner, self.background, self.sample_start, self.sample_length)
+    }
+
+}
+
 fn main() {
-    let buf = Vec::new();
-    let mut parser = OsuParser::new("assets/Wings_of_Justice/woj.osu".to_string(), buf, 0,);
+    let mut parser = OsuParser::new("assets/Wings_of_Justice/woj.osu".to_string());
     println!("Hello, world!");
     println!("File: {:?}", parser.get_file());
-    println!("File contents: {:?}", parser.read_file());
+    let file_data = parser.parse_file();
+    let song_details = parser.get_metadata(&file_data);
+    for line in song_details.iter() {
+        println!("{}", line);
+    };
+    println!("---------------------------");
+
+    let song = Song::new("WINGS OF JUSTICE".to_string(), "GALNERYUS".to_string(), "Sotarks".to_string(), "FLYING TOWARDS JUSTICE".to_string());
+    println!("{}", song.get_song_details());
 }
--- src/osu_parser.rs
+++ src/osu_parser.rs
@@ -4,35 +4,50 @@ use std::{fs::File, io::Read};
 
 pub struct OsuParser {
     file: String,
-    buffer: Vec<u8>,
-    cursor: usize,
 }
 
 impl OsuParser {
-    pub fn new(file: String, buffer: Vec<u8>, cursor: usize) -> Self {
+    pub fn new(file: String) -> Self {
         OsuParser {
             file,
-            buffer,
-            cursor,
         }
     }
 
     pub fn get_file(&self) -> String {
         self.file.clone()
-    }
-
-    pub fn get_buffer(&self) -> &Vec<u8> {
-        &self.buffer
-    }
-
-    pub fn get_cursor(&self) -> usize {
-        self.cursor
     } 
 
-    pub fn read_file(&mut self) -> String {
+    fn read_file(&mut self) -> String {
         let mut f = File::open(self.file.clone()).expect("Unable to open file");
         let mut data = String::new();
         f.read_to_string(&mut data).expect("Unable to read data");
         return data;
+    }
+
+    pub fn parse_file(&mut self) -> Vec<String> {
+        let data = self.read_file();
+        let collect = data.lines().map(|line| line.to_string()).collect::<Vec<String>>();
+        // for line in collect.iter() {
+        //     println!("{} ###", line);
+        // }
+        return collect;
+    }
+
+    // RELEVANT FIELDS (for ITG): Title, Artist, Creator, Version
+    pub fn get_metadata(&mut self, data: &Vec<String>) -> Vec<String> {
+        let mut song_details = vec![String::new()];
+        let mut iter = data.iter();
+
+        while let Some(line) = iter.next() {
+            if line.contains("[Metadata]") {
+                while let Some(metadata_line) = iter.next() {
+                    if metadata_line.contains("[") {
+                        break;
+                    }
+                    song_details.push(metadata_line.clone());
+                }
+            }
+        }
+        song_details
     }
 }
